2aed6bd8b418c243cf44d1dffbc216f390908ba1
pachoning <cc.pachon@gmail.com>
2024-06-19 22:47:39 +0200

    Update workflow script

--- utils/workflow.py
+++ utils/workflow.py
@@ -12,19 +12,21 @@ import numpy as np
 import os
 import pickle
 
-
-domain_range = (0, 1)
+get_lm_results = True
+get_knn_results = True
+get_fnn_results = True
 num_intervals = 20
 num_permutations = 10
+max_trials_fnn = 12
+domain_range = (0, 1)
+num_intervals = 20
+num_permutations = 1000
 simulated_data_path = os.path.join(data_path, "output")
-n_basis_representation = 10
+n_basis_representation = 11
 basis_bsplines = BSplineBasis(
     n_basis=n_basis_representation,
     domain_range=(0 ,1)
 )
-num_intervals = 20
-num_permutations = 10
-
 reg_list = [l2_reg(x) for x in np.arange(0.2, 1.5, 0.2)]
 reg_list.append(None)
 
@@ -64,143 +66,142 @@ for i_sim in range(num_simulations):
         X_test_bspline = X_test_grid.to_basis(basis_bsplines)
         X_full_bspline = X_full_grid.to_basis(basis_bsplines)
         ########## Linear model
-        print("\t\tFitting linear model")
-        hyperopt_lm = HyperOptScikitFda(
-            LinearRegression,
-            abscissa_points=abscissa_points,
-            domain_range=domain_range,
-        )
-        params_lm = {
-            "regularization": reg_list
-        }
-        hist_lm = hyperopt_lm.search(
-            params=params_lm,
-            X_train=X_train_bspline,
-            y_train=target_train[:, 0],
-            X_val=X_validation_bspline,
-            y_val=target_validation[:, 0]
-        )
-        best_params_lm = hist_lm.best_params_
-        best_model_lm = hyperopt_lm.cls_estimator(**best_params_lm)
-        _ = best_model_lm.fit(X_full_bspline, target_full[:, 0])
-        # Transform predict function to use a numpy array as input
-        pred_lm = predict_from_np(
-            grid_points=abscissa_points,
-            domain_range=domain_range,
-            basis=X_full_bspline.basis,
-            predict_fn=best_model_lm.predict
-        )
-        # Shapley for the Linear Model
-        print("\t\tComputing Shapley for linear model")
-        shapley_fda_lm = ShapleyFda(
-            predict_fn=pred_lm,
-            X=X_test,
-            abscissa_points=abscissa_points,
-            target=target_test[:, 0],
-            domain_range=domain_range,
-            verbose=False,
-        )
-        values_shapley_lm = shapley_fda_lm.compute_shapley_value(
-            num_intervals=num_intervals,
-            num_permutations=num_permutations,
-        )
-        values_shapley_lm_name = f"shapley_lm_{i_sim}.pkl"
-        values_shapley_lm_file = os.path.join(data_path, "output", scenario_path, values_shapley_lm_name)
-        with open(values_shapley_lm_file, 'wb') as f_lm:
-                pickle.dump(values_shapley_lm, f_lm)
+        if get_lm_results:
+            print("\t\tFitting linear model")
+            hyperopt_lm = HyperOptScikitFda(
+                LinearRegression,
+                abscissa_points=abscissa_points,
+                domain_range=domain_range,
+            )
+            params_lm = {
+                "regularization": reg_list
+            }
+            hist_lm = hyperopt_lm.search(
+                params=params_lm,
+                X_train=X_train_bspline,
+                y_train=target_train[:, 0],
+                X_val=X_validation_bspline,
+                y_val=target_validation[:, 0]
+            )
+            best_params_lm = hist_lm.best_params_
+            best_model_lm = hyperopt_lm.cls_estimator(**best_params_lm)
+            _ = best_model_lm.fit(X_full_bspline, target_full[:, 0])
+            # Transform predict function to use a numpy array as input
+            pred_lm = predict_from_np(
+                grid_points=abscissa_points,
+                domain_range=domain_range,
+                basis=X_full_bspline.basis,
+                predict_fn=best_model_lm.predict
+            )
+            # Shapley for the Linear Model
+            print("\t\tComputing Shapley for linear model")
+            shapley_fda_lm = ShapleyFda(
+                predict_fn=pred_lm,
+                X=X_test,
+                abscissa_points=abscissa_points,
+                target=target_test[:, 0],
+                domain_range=domain_range,
+                verbose=False,
+            )
+            values_shapley_lm = shapley_fda_lm.compute_shapley_value(
+                num_intervals=num_intervals,
+                num_permutations=num_permutations,
+            )
+            values_shapley_lm_name = f"shapley_lm_{i_sim}.pkl"
+            values_shapley_lm_file = os.path.join(data_path, "output", scenario_path, values_shapley_lm_name)
+            with open(values_shapley_lm_file, 'wb') as f_lm:
+                    pickle.dump(values_shapley_lm, f_lm)
         ########## KNN
-        print("\t\tFitting knn model")
-        hyperopt_knn = HyperOptScikitFda(
-             KNeighborsRegressor,
-             abscissa_points=abscissa_points,
-             domain_range=domain_range,
-        )
-        hist_knn = hyperopt_knn.search(
-             params={"n_neighbors": range(3, 12, 2)},
-             X_train=X_train,
-             y_train=target_train,
-             X_val=X_validation,
-             y_val=target_validation
-        )
-        best_params_knn = hist_knn.best_params_
-        best_model_knn = hyperopt_knn.cls_estimator(**best_params_knn)
-        _ = best_model_knn.fit(X_full, target_full)
-        # Shapley for the KNN
-        print("\t\tComputing Shapley for knn model")
-        shapley_fda_knn = ShapleyFda(
-             predict_fn=best_model_knn.predict,
-             X=X_test,
-             abscissa_points=abscissa_points,
-             target=target_test,
-             domain_range=domain_range,
-             verbose=False,
-        )
-        values_shapley_knn = shapley_fda_knn.compute_shapley_value(
-             num_intervals=num_intervals,
-             num_permutations=num_permutations,
-        )
-        values_shapley_knn_name = f"shapley_knn_{i_sim}.pkl"
-        values_shapley_knn_file = os.path.join(data_path, "output", scenario_path, values_shapley_knn_name)
-        with open(values_shapley_knn_file, 'wb') as f_knn:
-            pickle.dump(values_shapley_knn, f_knn)
-        
+        if get_knn_results:
+            print("\t\tFitting knn model")
+            hyperopt_knn = HyperOptScikitFda(
+                KNeighborsRegressor,
+                abscissa_points=abscissa_points,
+                domain_range=domain_range,
+            )
+            hist_knn = hyperopt_knn.search(
+                params={"n_neighbors": range(3, 12, 2)},
+                X_train=X_train,
+                y_train=target_train,
+                X_val=X_validation,
+                y_val=target_validation
+            )
+            best_params_knn = hist_knn.best_params_
+            best_model_knn = hyperopt_knn.cls_estimator(**best_params_knn)
+            _ = best_model_knn.fit(X_full, target_full)
+            # Shapley for the KNN
+            print("\t\tComputing Shapley for knn model")
+            shapley_fda_knn = ShapleyFda(
+                predict_fn=best_model_knn.predict,
+                X=X_test,
+                abscissa_points=abscissa_points,
+                target=target_test,
+                domain_range=domain_range,
+                verbose=False,
+            )
+            values_shapley_knn = shapley_fda_knn.compute_shapley_value(
+                num_intervals=num_intervals,
+                num_permutations=num_permutations,
+            )
+            values_shapley_knn_name = f"shapley_knn_{i_sim}.pkl"
+            values_shapley_knn_file = os.path.join(data_path, "output", scenario_path, values_shapley_knn_name)
+            with open(values_shapley_knn_file, 'wb') as f_knn:
+                pickle.dump(values_shapley_knn, f_knn)
         ########## FNN
-        print("\t\tFitting fnn model")
-        hyperopt_fnn = HyperOptFnn(
-            input_shape=(X_train.shape[1], 1),
-            resolution=X_train.shape[1]
-        )
+        if get_fnn_results:
+            print("\t\tFitting fnn model")
+            hyperopt_fnn = HyperOptFnn(
+                input_shape=(X_train.shape[1], 1),
+                resolution=X_train.shape[1]
+            )
 
-        tuner_fnn = hyperopt_fnn.build_tuner(
-            objective="val_loss",
-            max_trials=6,
-            overwrite=True,
-            directory=".",
-            project_name="tune_hypermodel",
-        )
+            tuner_fnn = hyperopt_fnn.build_tuner(
+                objective="val_loss",
+                max_trials=max_trials_fnn,
+                overwrite=True,
+                directory=".",
+                project_name="tune_hypermodel",
+            )
 
-        tuner_fnn.search(
-            X_train,
-            target_train,
-            epochs=10,
-            batch_size=32,
-            validation_data=(X_validation, target_validation),
-            verbose=False,
-        )
+            tuner_fnn.search(
+                X_train,
+                target_train,
+                validation_data=(X_validation, target_validation),
+                verbose=False,
+            )
 
-        best_model_fnn = HyperOptFnn(
-            input_shape=(X_train.shape[1], 1),
-            resolution=X_train.shape[1]
-        )
+            params_best_fnn = tuner_fnn.get_best_hyperparameters(1)[0]
+            best_epochs = params_best_fnn.get("epochs")
+            hyperopt_best_fnn = HyperOptFnn(
+                input_shape=(X_train.shape[1], 1),
+                resolution=X_train.shape[1]
+            )
 
-        best_params_fnn = tuner_fnn.get_best_hyperparameters(5)[0]
-        model_fnn = best_model_fnn.build(best_params_fnn)
-        history = best_model_fnn.fit(
-            best_params_fnn,
-            model_fnn,
-            X_full,
-            target_full,
-            batch_size=32,
-            epochs=1,
-            verbose=False,
-        )
-        # Shapley for FNN
-        print("\t\tComputing Shapley for fnn model")
-        shapley_fda_fnn = ShapleyFda(
-            predict_fn=predict_no_verbose(model_fnn.predict),
-            X=X_test,
-            abscissa_points=abscissa_points,
-            target=target_test,
-            domain_range=domain_range,
-            verbose=False,
-        )
+            model_fnn = hyperopt_best_fnn.build(params_best_fnn)
+            history = hyperopt_best_fnn.fit(
+                params_best_fnn,
+                model_fnn,
+                X_full,
+                target_full,
+                epochs=best_epochs,
+                verbose=False
+            )
+            # Shapley for FNN
+            print("\t\tComputing Shapley for fnn model")
+            shapley_fda_fnn = ShapleyFda(
+                predict_fn=predict_no_verbose(model_fnn.predict),
+                X=X_test,
+                abscissa_points=abscissa_points,
+                target=target_test,
+                domain_range=domain_range,
+                verbose=False,
+            )
 
-        values_shapley_fnn = shapley_fda_fnn.compute_shapley_value(
-            num_intervals=num_intervals,
-            num_permutations=num_permutations,
-        )
-        values_shapley_fnn_name = f"shapley_fnn_{i_sim}.pkl"
-        values_shapley_fnn_file = os.path.join(data_path, "output", scenario_path, values_shapley_fnn_name)
-        with open(values_shapley_fnn_file, 'wb') as f_knn:
-            pickle.dump(values_shapley_fnn, f_knn)
-        
+            values_shapley_fnn = shapley_fda_fnn.compute_shapley_value(
+                num_intervals=num_intervals,
+                num_permutations=num_permutations,
+            )
+            values_shapley_fnn_name = f"shapley_fnn_{i_sim}.pkl"
+            values_shapley_fnn_file = os.path.join(data_path, "output", scenario_path, values_shapley_fnn_name)
+            with open(values_shapley_fnn_file, 'wb') as f_knn:
+                pickle.dump(values_shapley_fnn, f_knn)
