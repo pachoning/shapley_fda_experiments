f8fa1386d06b50f20e2612123096f80e785e438e
pachoning <cc.pachon@gmail.com>
2024-06-25 06:37:40 +0200

    Add additional tools to analise the results

--- utils/utils_workflow.py
+++ utils/utils_workflow.py
@@ -1,5 +1,7 @@
 from skfda.misc.operators import LinearDifferentialOperator
 from skfda.misc.regularization import L2Regularization
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
@@ -29,6 +31,12 @@ def predict_no_verbose(predict_fn):
         return predict_fn(*args, verbose=False, **kwargs)
     return inner
 
+def predict_no_verbose_squeeze(predict_fn):
+    def inner(*args, **kwargs):
+        predicted_value = predict_fn(*args, verbose=False, **kwargs)
+        return predicted_value[:, 0]
+    return inner
+
 def l2_reg(lambda_value):
     operator = L2Regularization(
         linear_operator=LinearDifferentialOperator(2),
@@ -46,3 +54,64 @@ def obtain_score(prediction, target):
     tss = np.sum(diff_target_target_mean_sq)
     r2 = 1 - rss/tss
     return r2
+
+def plot_step_fn(
+    x_min,
+    x_max,
+    values,
+    colors,
+    models,
+    domain_range,
+    x_lab,
+    y_lab,
+    is_tecator=False,
+    plt_h_line=True,
+    plot_v_line=True,
+):
+    # Dividir por el ancho de cada intervalo de la particion
+    # Equivalentea a multiplicar por el nmero de intervalos y dividir por el ancho del intervalo grande (domain_range)
+    range_val = domain_range[1] - domain_range[0]
+    factor = len(x_max)/range_val
+    print("Num intervals:", len(x_max))
+    print("Range value:", range_val)
+    print("Factor:", factor)
+    #factor = 1
+    n_models = len(models)
+    if is_tecator:
+        plt.step(
+            x_min,
+            factor * values.T,
+            where="post",
+            label=models
+        )
+    else:
+        plt.step(
+        x_min,
+        factor * values.T,
+        where="post",
+        linestyle=(0, (1, 5)),
+        label=models
+    )
+    plt.legend(handles=[mpatches.Patch(color=col, label=mod) for col, mod in zip(colors, models)])
+    for i in range(n_models):
+        data_i = factor * values[i, :]
+        plt.hlines(
+            data_i,
+            x_min,
+            x_max,
+            color=colors[i],
+            linewidths=3
+        )
+    if plt_h_line:
+        plt.hlines(0, domain_range[0], domain_range[1], color='tab:grey')
+    if plot_v_line:
+        plt.vlines(0, 0, factor * np.max(values), color='tab:grey')
+    plt.xlabel(x_lab)
+    plt.ylabel(y_lab)
+
+def compute_mean_value(arr):
+    mean_val = [np.mean(x) for x in arr]
+    return mean_val
+
+def obtain_values_from_object(shapley_object):
+    return np.array([[x[1] for x in shapley_object]])
