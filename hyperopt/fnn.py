c35e2e29967b6684e465b560fad78d0e69e5236c
pachoning <cc.pachon@gmail.com>
2024-07-24 16:09:56 +0200

    Allow to tune more parameters in fnn

--- hyperopt/fnn.py
+++ hyperopt/fnn.py
@@ -8,36 +8,36 @@ class HyperOptFnn(keras_tuner.HyperModel):
         self.resolution = resolution
         super().__init__(self)
 
-    def build_tuner(
-        self,
-        max_trials,
-        objective="val_loss",
-        overwrite=True,
-        directory=".",
-        project_name="tune_hypermodel",
-    ):
+    def build_input_layer(self, wanted_normalisation):
+        input = tf.keras.layers.Input(shape=self.input_shape, name="input")
+        normalised = None
+        if wanted_normalisation:
+            norm_axes = list(range(len(self.input_shape) - 1))
+            normalised = tf.keras.layers.LayerNormalization(
+                axis=norm_axes,
+                center=False,
+                scale=False,
+                epsilon=1e-10,
+                name="normalization"
+            )(input)
+        return input, normalised
 
-        cutom_keras_tuner = keras_tuner.RandomSearch(
+    def build_hidden_dense_layers(
             self,
-            objective=objective,
-            max_trials=max_trials,
-            overwrite=overwrite,
-            directory=directory,
-            project_name=project_name,
-        )
-        return cutom_keras_tuner
-
-
-    def build_hidden_layers(self, hp, input_layer):
-        num_hidden_layers = hp.Int("hidden_layer", min_value=2, max_value=6, step=1)
+            input_layer,
+            num_hidden_layers,
+            n_bases,
+            bases_types,
+        ):
         layer_options = []
-        for i_layer_hp in range(num_hidden_layers):
+        for i_layer in range(num_hidden_layers):
+            basis_types = bases_types[i_layer]
             dict_layer = {
-                "n_neurons": hp.Int(f"units_{i_layer_hp}", min_value=5, max_value=15, step=1),
+                "n_neurons": n_bases[i_layer],
                 "basis_options": {
                     "n_functions": 6,
                     "resolution": self.resolution,
-                    "basis_type": "Legendre",
+                    "basis_type": basis_types,
                 },
                 "activation": "relu",
                 "pooling": False
@@ -48,57 +48,82 @@ class HyperOptFnn(keras_tuner.HyperModel):
         for i_layer, layer_option in enumerate(layer_options):
             layer = FunctionalDense(
                 **layer_option,
-                name=f"FunctionalDense_{i_layer}"
+                name=f"dense_{i_layer}"
             )(layer)
         return layer
 
     def build(self, hp):
-        input_layer = tf.keras.layers.Input(shape=self.input_shape)
-        #norm_axes = list(range(len(self.input_shape) - 1))
-        #norm_layer = tf.keras.layers.LayerNormalization(
-        #    axis=norm_axes,
-        #    center=False,
-        #    scale=False,
-        #    epsilon=1e-10,
-        #    name="Normalization"
-        #)(input_layer)
-        #hidden_layers = self.build_hidden_layers(hp=hp, input_layer=norm_layer)
-        hidden_layers = self.build_hidden_layers(hp=hp, input_layer=input_layer)
+        wanted_normalisation = hp.Boolean("wanted_normalisation")
+        input, normalised = self.build_input_layer(wanted_normalisation)
+        hidden_layer_input = input
+        if wanted_normalisation:
+            hidden_layer_input = normalised
+        num_hidden_layers = hp.Int("hidden_layer", min_value=2, max_value=6, step=1)
+        n_bases_hidden = [
+            hp.Int(f"units_{i_layer}", min_value=5, max_value=15, step=1) for i_layer in range(num_hidden_layers)
+        ]
+        bases_types_hidden = [
+            hp.Choice(f"basis_type_{i_layer}", ["Fourier", "Legendre"]) for i_layer in range(num_hidden_layers)
+        ]
+
+        hidden_dense_layers = self.build_hidden_dense_layers(
+            input_layer=hidden_layer_input,
+            num_hidden_layers=num_hidden_layers,
+            n_bases=n_bases_hidden,
+            bases_types=bases_types_hidden,
+        )
+        n_bases_output = hp.Int("units_output", min_value=1, max_value=15, step=1)
+        basis_type_output = hp.Choice("basis_type_output", ["Fourier", "Legendre"])
         output_layer_options = {
             "n_neurons": 1,
             "basis_options": {
-                "n_functions": 3,
+                "n_functions": n_bases_output,
                 "resolution": self.resolution,
-                "basis_type": "Fourier"
+                "basis_type": basis_type_output
             },
             "activation": "linear",
             "pooling": True
         }
         output_layer = FunctionalDense(
-                **output_layer_options,
-                name=f"OutputLayer"
-            )(hidden_layers)
-        model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
+            **output_layer_options,
+            name="output"
+        )(hidden_dense_layers)
+        model = tf.keras.Model(inputs=input, outputs=output_layer)
         model.compile(
             loss=tf.keras.losses.MeanSquaredError(),
             optimizer="adam",
         )
         return model
 
-    def fit(self, hp, model, X, y, validation_data=None, *args, **kwargs):
+    def fit(
+        self,
+        hp,
+        model,
+        X,
+        y,
+        epochs=None,
+        batch_size=None,
+        validation_data=None,
+        **kwargs
+    ):
+        if epochs is None:
+            epochs = hp.Choice("epochs", [40, 42, 44, 46, 50, 52, 54, 56, 58, 60])
+        if batch_size is None:
+            batch_size = hp.Choice("batch_size", [8, 16, 32, 64, 128])
         if validation_data:
             return model.fit(
-                *args,
-                X,
-                y,
+                x=X,
+                y=y,
                 validation_data=validation_data,
-                epochs=hp.Choice("epochs", [40, 42, 44, 46, 50, 52, 54, 56, 58, 60]),
+                epochs=epochs,
+                batch_size=batch_size,
                 **kwargs
             )
         else:
             return model.fit(
-                *args,
-                X,
-                y,
-                **kwargs
-            )
+            x=X,
+            y=y,
+            epochs=epochs,
+            batch_size=batch_size,
+            **kwargs
+        )
