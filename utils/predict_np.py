1633e7d6024163f94f9aa5f702af80c118f7e9d4
pachoning <cc.pachon@gmail.com>
2024-07-24 16:17:11 +0200

    Delete unnecessary files

--- utils/predict_np.py
+++
@@ -1,9 +0,0 @@
-from skfda.representation.grid import FDataGrid
-
-def predict_from_np(grid_points, domain_range, basis, predict_fn):
-    def inner_predict_from_np(X):
-        X_to_grid = FDataGrid(data_matrix=X, grid_points=grid_points, domain_range=domain_range)
-        X_to_basis = X_to_grid.to_basis(basis)
-        prediction = predict_fn(X_to_basis)
-        return prediction
-    return inner_predict_from_np
